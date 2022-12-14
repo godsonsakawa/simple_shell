535fa22f71142f2e840ba14cd59bf4cc3b9e597e
Godson Sakawa <godsonsakawa@gmail.com>
2022-12-14 12:47:54 +0300

    processes

--- pid.c
+++ pid.c
@@ -2,6 +2,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+/**
+  * main - check the code.
+  *
+  * Return: 0 always success.
+  */
 int main(void)
 {
 	printf(" Current Process: %d\n Parent Process: %d\n", getpid(), getppid());
